33bc61d4c4527cf396af53e552a8c40d42c2e4c9
tonypham90 <tuananh.gt@hotmail.com>
2021-07-08 18:02:21 +0700

    Enter

--- CSHARP/Program.cs
+++ CSHARP/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 
-namespace CSHARP
+namespace MyApplication
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            string userName = Console.ReadLine();
-            Console.WriteLine("Username is: " + userName);
-        }
+      // Type your username and press enter
+      Console.WriteLine("Enter username:");
+
+      // Create a string variable and get user input from the keyboard and store it in the variable
+      string userName = Console.ReadLine();
+
+      // Print the value of the variable (userName), which will display the input value
+      Console.WriteLine("Username is: " + userName);
     }
+  }
 }

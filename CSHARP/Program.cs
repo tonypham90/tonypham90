5d6c6ad9e9b676705fc51553dffaae13b4ceb73f
tonypham90 <tuananh.gt@hotmail.com>
2021-07-09 06:17:43 +0700

    enter

--- CSHARP/Program.cs
+++ CSHARP/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace MyApplication
+namespace CSHARP
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("I dont know how i did it");
+            Console.Write("exactly I dont know how it work");
+            string Name = Console.ReadLine();
+            Console.Write("Your Name is:")
         }
     }
 }
